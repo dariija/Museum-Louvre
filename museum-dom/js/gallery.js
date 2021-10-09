60c97d3ce43bfde6907541053994f743f64de848
dariija <darija.bakanova@gmail.com>
2021-10-09 08:33:39 +0300

    feat: implement slider work in welcome section, animation in gallery section, interactive map in contacts section

--- museum-dom/js/gallery.js
+++ museum-dom/js/gallery.js
@@ -1,9 +1,3 @@
-const galleryPicturesInner = document.querySelector('.gallery-pictures__inner');
-
-const galleryPicturesSrc = ['assets/img/gallery/gallery1.jpg', 'assets/img/gallery/gallery2.jpg', 'assets/img/gallery/gallery3.jpg', 'assets/img/gallery/gallery4.jpg','assets/img/gallery/gallery5.jpg',
-                          'assets/img/gallery/gallery6.jpg', 'assets/img/gallery/gallery7.jpg', 'assets/img/gallery/gallery8.jpg', 'assets/img/gallery/gallery9.jpg', 'assets/img/gallery/gallery10.jpg',
-                          'assets/img/gallery/gallery11.jpg', 'assets/img/gallery/gallery12.jpg', 'assets/img/gallery/gallery13.jpg', 'assets/img/gallery/gallery14.jpg', 'assets/img/gallery/gallery15.jpg'];
-
 function shuffleGallery( arraySrc) {
     shuffle(arraySrc).map( function(imgSrc) {
         let img = document.createElement('img');
@@ -22,4 +16,47 @@ function shuffle( array) {
     return array
 };
 
+function animGalleryScroll() {
+    galleryPictures.forEach(function(picture) {
+        let pictureHeight = picture.offsetHeight;
+        let picturePosOffsetY = picture.getBoundingClientRect().top + window.pageYOffset;
+        let startAnim = 5;
+        let pictureAnimPoint = window.innerHeight - pictureHeight/startAnim;
+        
+        if ( window.pageYOffset > (picturePosOffsetY - pictureAnimPoint)) {
+            picture.classList.add('show-picture');
+        } else picture.classList.remove('show-picture');
+    })
+};
+
+function debounce(func, wait, immediate) {
+    let timeout;
+  
+    return function executedFunction() {
+      const context = this;
+      const args = arguments;
+  
+      const later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+  
+      const callNow = immediate && !timeout;
+  
+      clearTimeout(timeout);
+  
+      timeout = setTimeout(later, wait);
+
+      if (callNow) func.apply(context, args);
+    };
+};
+
+const galleryPicturesInner = document.querySelector('.gallery-pictures__inner');
+const galleryPicturesSrc = ['assets/img/gallery/gallery1.jpg', 'assets/img/gallery/gallery2.jpg', 'assets/img/gallery/gallery3.jpg', 'assets/img/gallery/gallery4.jpg','assets/img/gallery/gallery5.jpg',
+                          'assets/img/gallery/gallery6.jpg', 'assets/img/gallery/gallery7.jpg', 'assets/img/gallery/gallery8.jpg', 'assets/img/gallery/gallery9.jpg', 'assets/img/gallery/gallery10.jpg',
+                          'assets/img/gallery/gallery11.jpg', 'assets/img/gallery/gallery12.jpg', 'assets/img/gallery/gallery13.jpg', 'assets/img/gallery/gallery14.jpg', 'assets/img/gallery/gallery15.jpg'];
+
 shuffleGallery( galleryPicturesSrc);
+
+let galleryPictures = document.querySelectorAll('.gallery-pictures__image');
+window.addEventListener('scroll',  debounce(animGalleryScroll, 20, true));
